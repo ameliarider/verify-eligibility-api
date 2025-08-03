c9630dc288a8a57a61839b8967423f0f73e92b44
ameliarider <mimirider89@gmail.com>
2025-08-02 20:39:58 -0700

    updating login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "/eligibility_check" => "eligibility_check#index"
   get "/verify" => "eligibility_check#show"
 
-  post "/sessions" => "sessions#create"
-  delete "/sessions" => "sessions#destroy"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
 end
