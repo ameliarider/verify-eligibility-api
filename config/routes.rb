bf47f76e9e088ae9c2a4a9adba49c84cf0dd58f1
ameliarider <mimirider89@gmail.com>
2025-08-02 20:33:39 -0700

    Adding backend cookie authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/eligibility_check" => "eligibility_check#index"
   get "/verify" => "eligibility_check#show"
+
+  post "/sessions" => "sessions#create"
+  delete "/sessions" => "sessions#destroy"
 end
